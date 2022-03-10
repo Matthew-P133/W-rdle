afee569d9d2a3f59287277b19c09a3f71a1574ff
WZYGLA <2527931w@student.gla.ac.uk>
2022-03-10 19:05:35 +0000

    fixed userprofile and test

--- wordgame/models.py
+++ wordgame/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 from django.contrib.auth.models import User
 # Create your models here.
-from django.core.exceptions import ObjectDoesNotExist
+
 
 class UserProfile(models.Model):
     SEX_CHOICES = (
